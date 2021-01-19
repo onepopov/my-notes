85a4d28c49ea69459dd204747b2862dbd08da874
Alexandr Popov <popov.exceedteam@gmail.com>
2021-01-19 15:59:37 +0300

    fix: selectedNote

--- src/components/Content/index.js
+++ src/components/Content/index.js
@@ -9,17 +9,18 @@ import types from "../../store/types/notes";
 function Content() {
 
     const notes = useSelector((state) => state.notes);
-    const [text, setText] = useState(notes.notesList.find((note => note.id === notes.selectedNote)).content);
+    const selectedNote = notes.notesList.find((note) => note.id === notes.selectedNote);
+    const [text, setText] = useState(selectedNote ? selectedNote.content: "");
     const dispatch = useDispatch();
     const debouncedText = useDebounce(text, 300);
     const handlerInput = (text) => {
       setText(text);
     };
     useEffect(() => {
-        setText(notes.notesList.find((note => note.id === notes.selectedNote)).content);
+        selectedNote && setText(selectedNote.content);
     }, [notes]);
     useEffect(() => {
-        if(text !== notes.notesList.find((note => note.id === notes.selectedNote)).content) {
+        if(selectedNote && text !== selectedNote.content) {
             dispatch({type: types.SET_CONTENT, payload: {id: notes.selectedNote,content: debouncedText}});
         }
     }, [debouncedText]);
